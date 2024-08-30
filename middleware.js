37071c876150f2638498102f64992a521c8785ba
yeyulchoi <yeyulchoi@outlook.com>
2024-08-30 19:10:47 -0400

    added protectd routes functionality

--- middleware.js
+++ middleware.js
@@ -1,1 +1,6 @@
 export{ default} from 'next-auth/middleware';
+
+//all routes to be protected
+export const config ={
+    matcher : ['/properties/add', '/profile', '/properties/saved', '/messages']
+}
