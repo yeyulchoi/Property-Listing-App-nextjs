c48647baad1d26eb1d49c5db0b5d25a17f1a03a9
yeyulchoi <yeyulchoi@outlook.com>
2024-09-02 20:11:02 -0400

    added functionality of Mark_As_Read

---
+++ app/actions/markMessageAsRead.js
@@ -0,0 +1,36 @@
+'use server'
+import connectDB from "@/config/database"
+import Message from "@/Models/Message"
+import { getSessionUser } from "@/utils/getSessionUser"
+import { revalidatePath } from "next/cache"
+
+async function markMessageAsRead(messageId){
+    await connectDB()
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId){
+        throw new Error('User Id is required')
+    }
+
+    const {userId}=sessionUser;
+
+    const message = await Message.findById(messageId)
+
+    if(!message) throw new Error('Message not found.')
+
+    // verify owership
+    if(message.recipient.toString() !== userId){
+        throw new Error('Unauthorized');
+    }
+
+    message.read = !message.read;
+
+    revalidatePath('/messages','page');
+
+    await message.save();
+
+    return message.read;
+}
+
+export default markMessageAsRead
--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -1,9 +1,29 @@
 
+'use client'
+
+import { useState } from "react";
+import { toast } from "react-toastify";
+import markMessageAsRead from "@/app/actions/markMessageAsRead";
+
 
 
 const MessageCard = ({message}) => {
+    const [isRead, setIsRead] = useState(message.read);
+
+    const handleReadClick= async()=>{
+        const read = await markMessageAsRead(message._id)
+
+        setIsRead(read);
+        toast.success(`Marked as ${read? 'read':'new'}`)
+    }
+
     return ( 
         <div className="relative bg-white-p4 rounded-md shadow-md border border-gray-200">
+            {!isRead &&(
+                <div className="absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md">
+                    New
+                </div>
+            )}
             <h2 className="text-xl mb-4">
                 <span className="font-bold">Property Inquiry:</span>
                 {message.property.name}
@@ -27,8 +47,8 @@ const MessageCard = ({message}) => {
                     {new Date(message.createdAt).toLocaleString()}
                 </li>
             </ul>
-            <button className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md">
-                Mark as Read
+            <button onClick={handleReadClick}  className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md">
+                {isRead? 'Mark As New':'Mark as Read'}
             </button>
             <button className="mt-4 mr-3 bg-red-500 text-white py-1 px-3 rounded-md">
                 Delete
