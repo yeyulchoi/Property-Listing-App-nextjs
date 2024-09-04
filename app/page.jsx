e8a8e3e1a2411090d37f5c575e5fc556d0f821eb
yeyulchoi <yeyulchoi@outlook.com>
2024-09-03 20:15:09 -0400

    added featuredProperties

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from '@/components/HomeProperties'
 import connectDB from '@/config/database'
+import FeaturedPRoperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
     
@@ -9,6 +10,7 @@ const HomePage = () => {
        <>
        <Hero />
        <InfoBoxes />
+       <FeaturedPRoperties />
        <HomeProperties />
        
        </>
